d6094486ff22f0f9eb75071acb0c543e9ec81fb9
pvDung <dungvatoi12@gmail.com>
2017-08-09 17:02:08 +0700

    add user validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731035524) do
+ActiveRecord::Schema.define(version: 20170809073443) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
 end
